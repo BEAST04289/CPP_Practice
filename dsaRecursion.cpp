0eed8892739ae49c102e9f8764208988d310da44
BEAST04289 <shaurya04289@gmail.com>
2025-10-12 01:36:26 +0530

    recursion update

--- dsaRecursion.cpp
+++ dsaRecursion.cpp
@@ -40,4 +40,226 @@ int main() {
 
 */
 
-//
+//PRINT NUMBERS FROM 1 TO N USING RECURSION
+
+/*
+#include <bits/stdc++.h>
+using namespace std; 
+
+void print(int i, int n) {
+    if (i > n) {
+        return;
+    }
+    cout << i << " ";
+    print(i + 1, n);
+}
+int main() {
+    int n;
+    cin >> n;
+    print(1, n);
+    return 0;
+}
+
+*/
+
+//PRINT NUMBERS FROM N TO 1 USING RECURSION
+
+/*
+#include <bits/stdc++.h>
+using namespace std; 
+
+void print(int n, int i=1) {
+    if (n==0 ) {
+        return;
+    }
+    cout << i << " ";
+    print(n ,i--);
+ 
+}
+int main() {
+    int n;
+    cin >> n;
+    print(n , n);
+    return 0;
+}
+*/
+
+//PRINT FROM 1 TO N USING RECURSION (BACKTRACKING)
+
+/*
+#include <bits/stdc++.h>
+using namespace std; 
+
+void print(int n,int i =1) {
+    if(i==n+1) return;
+ cout << i << " ";
+ print(n,i+1);
+ 
+}
+int main() {
+    int n;
+    cin >> n;
+    print(n);
+    return 0;
+}
+    */
+
+//PRINT FROM N TO 1 USING RECURSION (BACKTRACKING)
+
+/*
+#include <bits/stdc++.h>
+using namespace std; 
+
+void print(int n) {
+    if (n == 0) {
+        return;
+    }
+    cout << n << " ";
+    print(n - 1);
+}
+int main() {
+    int n;
+    cin >> n;
+    print(n);
+    return 0;
+}
+*/
+
+// SUM OF FIRST N NATURAL NUMBERS USING RECURSION
+
+//Parameterised Recursion
+
+/*
+#include<bits/stdc++.h>
+using namespace std;
+// we want the function to print the sum and not return it
+void sum(int n, int i,  int s) { // non initialised
+    if (i == n + 1) {
+        cout << "sum = " << s << endl;
+        return;
+    }
+    s = s + i;
+    sum(n, i + 1, s);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    sum(n,1,0); //another way other than using global variable
+    return 0;
+}
+
+*/
+
+
+//Functional Recursion
+// we want function to return not print the sum
+
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+int sum(int n) {  //f(n) = n + f(n-1)
+     //f(3) = 3 + f(2)
+     //f(2) = 2 + f(1)
+     //f(1) = 1 + f(0)
+     //f(0) = 0
+     //f(1) = 1 + 0 = 1
+     //f(2) = 2 + 1 = 3
+     //f(3) = 3 + 3 = 6
+     // so sum of first n natural numbers is n*(n+1)/2
+    if (n == 0) {
+        return 0;
+    }
+    return n + sum(n - 1);
+}
+   int main() {
+ int n;
+ cin>>n;
+ sum(n);
+ cout << "sum is " << sum(n); //important to use cout here to print the returned value
+ return 0;
+
+   }
+
+   */
+
+   //REVERSE AN ARRAY USING RECURSION
+/*
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void rev(int i,int arr[],int n) {
+    if (i>=n/2){
+        return ;
+    }
+    swap(arr[i],arr[n-i-1]);
+    rev(i+1,arr,n);
+    
+}
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i=0;i<n;i++) {
+        cin >> arr[i];
+    }
+ rev(0,arr,n);
+for(int i=0;i<n;i++){
+    cout << arr[i] << " ";
+
+ 
+}
+return 0;
+}
+*/
+
+//CHECK IF STRING IS PALINDROME OR NOT
+
+/*
+#include <bits/stdc++.h>
+using namespace std;
+ 
+bool pal(int i,string &s) {
+if(i>=s.size()/2) 
+    return true;
+    if(s[i] != s[s.size() - i - 1])
+        return false;
+    return pal(i+1,s);
+
+}
+int main() {
+    string s="adda";
+    cout << pal(0,s);
+
+return 0;
+}
+ */
+
+ //FIBONACCI SERIES USING RECURSION
+
+ /*
+ #include<bits/stdc++.h>
+    using namespace std;
+
+    int fib(int n) {
+       if(n==0) return 0;
+       if(n==1) return 1;
+         //fib(2)=fib(1)+fib(0)=1+0=1
+         //fib(3)=fib(2)+fib(1)=1+1=2
+         //fib(4)=fib(3)+fib(2)=2+1=3
+         //fib(5)=fib(4)+fib(3)=3+2=5
+         // so on
+        if(n<=1) return n;
+        return fib(n-1) + fib(n-2);
+
+    }
+
+    int main() {
+        int n;
+        cin >> n;
+        cout << fib(n);
+        return 0;
+    }
+
+    */
